f0342cab13b8a5ee2bd60e8a1ea3a30eb3be21d0
Shariar Imam Shafin <imanshariar@gmail.com>
2025-05-19 15:15:57 +0600

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,9 +1,9 @@
 # Wi-Fi credentials
 SSID = "YOUR_WIFI_SSID"
-PASSWORD = "WIFI_PASSWORD"
+PASSWORD = "YOUR_WIFI_PASSWORD"
 
 # Open-Meteo API URL
 WEATHER_URL = "https://api.open-meteo.com/v1/forecast?latitude=23.8429&longitude=90.3736&current=temperature_2m,relative_humidity_2m,weather_code"
 
 # ThingSpeak API key
-THINGSPEAK_API_KEY = "Your_ThingSpeak_API"
+THINGSPEAK_API_KEY = "YOUR_THINGSPEAK_API_KEY"
